06eacc3703812216355a3f950bd06bfcdbe25a02
killerted2525 <teddy.higgins@icloud.com>
2025-11-05 18:51:22 -0500

    Delete AllergyNutrition directory

--- AllergyNutrition/client/src/features/auth/index.ts
+++
@@ -1,4 +0,0 @@
-// Auth feature barrel exports
-export { default as Landing } from './pages/landing';
-export { useAuth } from './hooks/useAuth';
-export { isUnauthorizedError } from './lib/authUtils';
